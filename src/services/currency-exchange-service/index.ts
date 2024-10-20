1a38160e4691183cc1acc1ff6bd69c8fbac2f34d
Javier Fray <javier.fray@dacodes.com.mx>
2024-10-19 22:26:27 -0500

    ADD AmdorenProvider to currencyController

--- src/services/currency-exchange-service/index.ts
+++ src/services/currency-exchange-service/index.ts
@@ -1,10 +1,19 @@
 import { query, Router } from "express";
 import CurrencyController from "./controller";
+import { AmdorenProvider } from "./api-providers";
+import dotEnv from "dotenv"
+
+dotEnv.config();
+
+const AMDOREN_BASE_URI = process.env.AMDOREN_BASE_URI || "";
+const AMDOREN_API_TOKEN = process.env.AMDOREN_API_TOKEN || "";
+
+const currencyController = new CurrencyController(new AmdorenProvider(AMDOREN_API_TOKEN, AMDOREN_BASE_URI));
 
 const currencyRouter = Router();
 
 currencyRouter.get("/", async (_req, res) => {
-    const message = await CurrencyController.Get();
+    const message = currencyController.Get();
     res.send(message);
 });
 
@@ -12,7 +21,7 @@ currencyRouter.get("/getCurrency", async (req, res) => {
     const from = typeof req.query.from === "string" ? req.query.from : "";
     const to = typeof req.query.to === "string" ? req.query.to : "";
     const amount = typeof req.query.amount === "string" ? req.query.amount : "";
-    const result = await CurrencyController.GetCurrency(from, to, amount);
+    const result = await currencyController.GetCurrency(from, to, amount);
     res.status(result.status).send(result)
 })
 
